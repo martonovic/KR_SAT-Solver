7ae02a1a2256d3b1cb993abea74c1b441223be08
martonovic <70942971+martonovic@users.noreply.github.com>
2021-11-23 02:19:17 +0100

    Create concatenate.py

---
+++ concatenate.py
@@ -0,0 +1,25 @@
+import glob
+import os
+
+
+def concat(rules, location, destination):
+    file_loc = os.path.join(os.getcwd(), location)
+    os.chdir(file_loc)
+
+    # read in all txt files
+    files = glob.glob(os.path.join('sudoku*.txt'))
+
+    # find the index of the sudoku rules
+    rules_index = files.index(rules)
+
+    # create new sudoku_test (rules + puzzle) for each puzzle
+    for index_file, this_file in enumerate(files):
+        if this_file != rules:
+            with open(os.path.join(destination, "sudoku_test{}.txt").format(index_file), "wb") as outfile:
+                with open(files[rules_index], "rb") as rules:
+                    outfile.write(rules.read())
+                with open(this_file, "rb") as infile:
+                    outfile.write(infile.read())
+
+
+concat('sudoku-rules-16x16.txt', 'tests/test2/', 'out')
